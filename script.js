871c4562b25204b62eda4913f567ab326eb72e93
lucasantos-S <lucassantos.dsilv@gmail.com>
2021-08-18 20:29:09 -0300

    incindo o estudo de objeto

---
+++ script.js
@@ -0,0 +1,16 @@
+class Produto {
+    constructor() {
+
+    }
+    adicionar() {
+        alert('botao adicionado')
+    }
+
+    excluir(){
+        alert('produto excluido')
+    }
+
+
+}
+
+let produto = new Produto()
