7dbe035816cfba490eead87301054a0bf7f7730c
lucasantos-S <lucassantos.dsilv@gmail.com>
2021-08-18 22:38:40 -0300

    incluido itens no array

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
 class Produto {
     constructor() {
 
+        this.id = 1;
+        this.arrayProdutos = [];
     }
-    adicionar() {
-        alert('botao adicionado')
+    salvar() {
+        let produto = this.lerDados();
+
+        if(this.validaCampos(produto)){
+            this.adicionar(produto);
+        }
+
+         console.log(this.arrayProdutos);
     }
 
-    excluir(){
-        alert('produto excluido')
+    adicionar(produto) {
+        this.arrayProdutos.push(produto);
+        this.id++;
+    }
+
+
+    lerDados(){
+        let produto = {}
+        produto.id = this.id;
+        produto.nomeProduto = document.getElementById('produto').value;
+        produto.valor = document.getElementById('valor').value;
+
+        return produto;
+    }
+    validaCampos(produto){
+        let msg = '';
+
+        if(produto.nomeProduto == '') {
+        msg += '-Informe o nome do produto \n'
+    }
+        if(produto.valor == '') {
+        msg += '-Informe o valor do produto \n'
+    }
+
+    if(msg != '') {
+        alert(msg);
+        return false
+    }
+    return true;
+    }
+
+    limpar(){
+       
     }
 
 
